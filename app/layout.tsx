f8095df644875444799a01de2d48128ff9911882
QuanTH02 <anhquankaka113@gmail.com>
2025-09-14 17:59:05 +0700

    Add GSC

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,17 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const googleSiteVerification = process.env.GOOGLE_SITE_VERIFICATION!;
+
 export const metadata: Metadata = {
   title: 'Luyện toán',
   description:
     'Ứng dụng luyện tập các phép tính cộng, trừ, nhân, chia cho học sinh',
   icons: {
     icon: '/logo.png',
+  },
+  verification: {
+    google: googleSiteVerification,
   },
 };
 
